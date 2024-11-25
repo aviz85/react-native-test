4ec8fff821cc13f5f5eab1000638cb7163f15778
Aviz85 <avizmaeir@gmail.com>
2024-11-25 15:07:26 +0200

    deleted app.json

---
+++ app.config.js
@@ -0,0 +1,38 @@
+export default {
+  expo: {
+    name: "React Native Test",
+    slug: "react-native-test",
+    version: "1.0.0",
+    orientation: "portrait",
+    icon: "./assets/icon.png",
+    userInterfaceStyle: "light",
+    splash: {
+      image: "./assets/splash.png",
+      resizeMode: "contain",
+      backgroundColor: "#ffffff"
+    },
+    assetBundlePatterns: [
+      "**/*"
+    ],
+    ios: {
+      supportsTablet: true
+    },
+    android: {
+      adaptiveIcon: {
+        foregroundImage: "./assets/adaptive-icon.png",
+        backgroundColor: "#ffffff"
+      },
+      package: "com.yourname.reactnativetest"
+    },
+    web: {
+      favicon: "./assets/favicon.png"
+    },
+    extra: {
+      supabaseUrl: process.env.EXPO_PUBLIC_SUPABASE_URL,
+      supabaseAnonKey: process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY,
+      eas: {
+        projectId: "0600d434-b17b-4920-8378-77795039d655"
+      }
+    }
+  }
+}; 
